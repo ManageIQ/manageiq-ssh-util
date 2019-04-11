7f2bdde38a9fa1fa199535e49045d1e4bdb360d0
Daniel Berger <djberg96@gmail.com>
2019-04-11 07:41:24 -0400

    Initial attempt at get_file specs.

--- spec/lib/manageiq/ssh/util_spec.rb
+++ spec/lib/manageiq/ssh/util_spec.rb
@@ -1,28 +1,45 @@
 require 'manageiq/ssh/util'
 
-describe ManageIQ::SSH::Util do
-  context "#temp_cmd_file" do
+RSpec.describe ManageIQ::SSH::Util do
+  context "#get_file" do
+    let(:ssh_util) { described_class.new('localhost', 'temp', 'something') }
+    let(:ssh_session) { double(Net::SSH::Connection::Session) }
+    let(:sftp_session) { double(Net::SFTP::Session) }
+    let(:sftp_download) { double(Net::SFTP::Operations::Download) }
+
     before do
-      @ssh_util = ManageIQ::SSH::Util.new("localhost", "temp", "something")
+      allow(ssh_util).to receive(:run_session).and_return(ssh_session)
+      allow(ssh_session).to receive(:sftp).and_return(sftp_session)
     end
+
+    it "retrieves a remote file" do
+      allow(sftp_session).to receive(:download!).and_return(sftp_download)
+      #expect(ssh_session.sftp).to eql(sftp_session)
+      #expect(sftp_session.download!('foo', 'bar')).to eql(sftp_download)
+      expect(ssh_util.get_file('remote_file', 'local_file')).to eql(sftp_download)
+    end
+  end
+
+  context "#temp_cmd_file" do
+    let(:ssh) { ManageIQ::SSH::Util.new("localhost", "temp", "something") }
 
     it "creates a file" do
       count = Dir.glob("/var/tmp/miq-*").size
 
-      @ssh_util.temp_cmd_file("pwd") do |_cmd|
+      ssh.temp_cmd_file("pwd") do |_cmd|
         expect(Dir.glob("/var/tmp/miq-*").size).to eq(count + 1)
       end
     end
 
     it "writes to file" do
-      @ssh_util.temp_cmd_file("pwd") do |cmd|
+      ssh.temp_cmd_file("pwd") do |cmd|
         expect(File.read(cmd.split(";")[1].strip)).to eq("pwd")
       end
     end
 
     it "deletes the file" do
       count = Dir.glob("/var/tmp/miq-*").size
-      @ssh_util.temp_cmd_file("pwd") {}
+      ssh.temp_cmd_file("pwd") {}
 
       expect(Dir.glob("/var/tmp/miq-*").size).to eq(count)
     end
