fd18c4df6aaf9af038840040b0c0abcf853395e0
Daniel Berger <djberg96@gmail.com>
2019-04-11 07:41:00 -0400

    Disable monkey patching, verify partial doubles and doubled constants.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,9 @@
 Dir[Rails.root.join("spec/shared/**/*.rb")].each { |f| require f }
+
+RSpec.configure do |config|
+  config.disable_monkey_patching!
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+    mocks.verify_doubled_constant_names = true
+  end
+end
