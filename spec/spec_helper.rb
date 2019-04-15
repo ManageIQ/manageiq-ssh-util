99821f0d5943b83367dc5f4659a9ac8b2e9eb87d
Daniel Berger <dberger@redhat.com>
2019-04-15 17:15:44 -0400

    Add persistence path.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,9 @@
 Dir[Rails.root.join("spec/shared/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
+  config.example_status_persistence_file_path = 'tmp/rspec_example_store.txt'
   config.disable_monkey_patching!
+
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
     mocks.verify_doubled_constant_names = true
