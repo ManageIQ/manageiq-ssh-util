7026ed745341708027588fe48fd25885734779cd
Daniel Berger <dberger@redhat.com>
2019-04-16 07:32:07 -0400

    Add focused context support.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 Dir[Rails.root.join("spec/shared/**/*.rb")].each { |f| require f }
 
 RSpec.configure do |config|
+  config.filter_run_when_matching(focus: true)
   config.example_status_persistence_file_path = 'tmp/rspec_example_store.txt'
   config.disable_monkey_patching!
 
