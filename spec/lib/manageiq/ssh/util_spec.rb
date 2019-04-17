d60b1306773b271deeffe973c0d742bc72ad1676
Daniel Berger <dberger@redhat.com>
2019-04-17 09:43:55 -0400

    Reworked context, added some log tests for get_file.

--- spec/lib/manageiq/ssh/util_spec.rb
+++ spec/lib/manageiq/ssh/util_spec.rb
@@ -1,21 +1,35 @@
 require 'manageiq/ssh/util'
 
 RSpec.describe ManageIQ::SSH::Util do
-  context "#get_file" do
-    let(:ssh_util) { described_class.new('localhost', 'temp', 'something') }
-    let(:ssh_session) { double(Net::SSH::Connection::Session) }
-    let(:sftp_session) { double(Net::SFTP::Session) }
-    let(:sftp_download) { double(Net::SFTP::Operations::Download) }
+  let(:host) { 'localhost' }
+  let(:ssh_session) { double(Net::SSH::Connection::Session) }
+  let(:sftp_session) { double(Net::SFTP::Session) }
+  let(:sftp_download) { double(Net::SFTP::Operations::Download) }
+  let(:ssh_util) { described_class.new(host, 'temp', 'something') }
+  let(:logger_file) { StringIO.new }
 
-    before do
-      allow(ssh_util).to receive(:run_session).and_yield(ssh_session)
-      allow(ssh_session).to receive(:sftp).and_return(sftp_session)
-      allow(sftp_session).to receive(:download!).and_return(sftp_download)
-    end
+  before do
+    $log = Logger.new(logger_file)
+    allow(ssh_util).to receive(:run_session).and_yield(ssh_session)
+    allow(ssh_session).to receive(:sftp).and_return(sftp_session)
+    allow(sftp_session).to receive(:download!).and_return(sftp_download)
+  end
+
+  context "#get_file" do
+    let(:from) { 'remote_file' }
+    let(:to) { 'local_file' }
 
     it "retrieves a remote file" do
       allow(sftp_session).to receive(:download!).and_return(sftp_download)
-      expect(ssh_util.get_file('remote_file', 'local_file')).to eql(sftp_download)
+      expect(ssh_util.get_file(from, to)).to eql(sftp_download)
+    end
+
+    it "writes the expected message to the log file" do
+      ssh_util.get_file(from, to)
+      logger_file.rewind
+      logger_contents = logger_file.read
+      expect(logger_contents).to include("MiqSshUtil::get_file - Copying file #{host}:#{from} to #{to}.")
+      expect(logger_contents).to include("MiqSshUtil::get_file - Copying of #{host}:#{from} to #{to}, complete.")
     end
   end
 
