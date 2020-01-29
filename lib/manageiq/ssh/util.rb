a2e794d78e9de2bbada28da242853636f28220a7
Daniel Berger <djberg96@gmail.com>
2020-01-29 07:04:31 -0500

    Update shell_with_su comments.

--- lib/manageiq/ssh/util.rb
+++ lib/manageiq/ssh/util.rb
@@ -339,8 +339,7 @@ module ManageIQ
       # remaining options are passed normally.
       #
       # This method is functionally identical to the following code, except that it
-      # yields itself (and nil) and re-raises certain Net::SSH exceptions as
-      # ManageIQ exceptions.
+      # yields itself (and nil).
       #
       #   ManageIQ::SSH::Util.new(host, remote_user, remote_password, {:su_user => su_user, :su_password => su_password})
       #
